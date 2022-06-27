99f723896135cd3bac648c8692cc574d4266f22d
Stilledood <74854275+Stilledood@users.noreply.github.com>
2022-06-27 13:47:16 +0300

    Added api root view

--- drone/drones/views.py
+++ drone/drones/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from .serializers import DroneCategorySerializer,DroneSerializer,PilotSerializer,CompetitionSerializer,PilotCompetitionsSerializer
 from rest_framework import generics
 from .models import *
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 
 class DroneCategoryList(generics.ListCreateAPIView):
 
     queryset = DroneCategory.objects.all()
     serializer_class = DroneCategorySerializer
+
 
 
 
@@ -22,6 +25,7 @@ class DroneList(generics.ListCreateAPIView):
 
     queryset = Drone.objects.all()
     serializer_class = DroneSerializer
+    name='drone-list'
 
 
 class DroneDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -35,6 +39,7 @@ class PilotList(generics.ListCreateAPIView):
 
     queryset = Pilot.objects.all()
     serializer_class = PilotSerializer
+    name='pilot-list'
 
 
 class PilotDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -56,5 +61,20 @@ class CompetitionDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Competition.objects.all()
     serializer_class = PilotCompetitionsSerializer
     name='competition-detail'
+
+
+class ApiRoot(generics.GenericAPIView):
+    name='api-root'
+
+    def get(self,request,*args,**kwargs):
+        return Response(
+            {
+                'drone-categories':reverse('category_list',request=request),
+                'drones':reverse(DroneList.name,request=request),
+                'pilots':reverse(PilotList.name,request=request),
+                'competitions':reverse(CompetitionList.name,request=request)
+
+            }
+        )
 
 
